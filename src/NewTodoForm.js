b964d428dbc7ab1d378c405bd6935e53212e55e4
Rana Bhattacharyya <ranab33@gmail.com>
2019-09-17 12:03:25 -0400

    css changes to forms

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -23,7 +23,7 @@ class NewTodoForm extends Component {
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form className="NewTodoForm" onSubmit={this.handleSubmit}>
                 <label htmlFor="task">New Todo</label>
                 <input
                     type="text"
